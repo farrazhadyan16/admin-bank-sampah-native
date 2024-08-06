4f6667af9c63c2b274397ecdd7c0a89eb9f54b5b
Farraz Hadyan <farrazhadyan16@gmail.com>
2024-08-06 22:11:12 +0700

    beli beli

--- pembelian.php
+++ pembelian.php
@@ -15,7 +15,7 @@ require_once 'crud-konsesi.php';
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>PMElectric | Input Konsesi</title>
+    <title>Bank Sampah | Pembelian</title>
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -53,8 +53,8 @@ require_once 'crud-konsesi.php';
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
-                    <h1 class="h3 mb-2 text-gray-800">Form Konsesi</h1>
-                    <p class="mb-4">Mohon Isi Data Konsesi pada Form di Bawah ini.
+                    <h1 class="h3 mb-2 text-gray-800">Form Pembelian </h1>
+                    <p class="mb-4">Di Form ini akan melakukan transaksi pembelian sampah dari user.
                     </p>
 
                     <form method="POST" action="">
@@ -62,13 +62,20 @@ require_once 'crud-konsesi.php';
                         <div class="row">
 
                             <!-- Border Left -->
-                            <div class="col-lg-6">
+                            <div class="col">
 
                                 <div class="card mb-4 py-3">
                                     <div class="card-body">
-                                        <label class="control-label col-sm-4">NO</label>
-                                        <div class="col-sm-8">
+                                        <label class="control-label col-sm-4">Id Nasabah</label>
+                                        <div class="col">
                                             <input type="int" class="form-control" placeholder="Masukkan No"
+                                                name="id_konsesi" required>
+                                        </div>
+                                        <br>
+                                        <label class="control-label col-sm-4">Nama</label>
+                                        <div class="col">
+                                            <input type="int" class="form-control"
+                                                placeholder="Otomatis Generate Nama setelah masukkan id/username nasabah"
                                                 name="id_konsesi" required>
                                         </div>
                                     </div>
@@ -76,172 +83,34 @@ require_once 'crud-konsesi.php';
 
                                 <div class="card mb-4 py-3">
                                     <div class="card-body">
-                                        <label class="control-label col-sm-4">JO</label>
-                                        <div class="col-sm-8">
+                                        <label class="control-label col-sm-4">Jenis Sampah</label>
+                                        <div class="col">
                                             <input type="text" class="form-control" placeholder="Masukkan JO" name="jo"
                                                 required>
                                         </div>
-                                    </div>
-                                </div>
-
-                                <div class="card mb-4 py-3">
-                                    <div class="card-body">
-                                        <label class="control-label col-sm-4">WO</label>
-                                        <div class="col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Masukkan WO" name="wo"
+                                        <br>
+                                        <label class="control-label col-sm-4">Berat</label>
+                                        <div class="col">
+                                            <input type="int" class="form-control" placeholder="Masukkan No"
+                                                name="id_konsesi" required>
+                                        </div>
+                                        <br>
+                                        <label class="control-label col-sm-4">Harga/gr</label>
+                                        <div class="col">
+                                            <input type="int" class="form-control"
+                                                placeholder="Otomatis Generate Harga dari database" name="id_konsesi"
                                                 required>
                                         </div>
-                                    </div>
-                                </div>
-
-                                <div class="card mb-4 py-3">
-                                    <div class="card-body">
-                                        <label class="control-label col-sm-4">Nama Project</label>
-                                        <div class="col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Masukkan Nama Project"
-                                                name="nama_project" required>
+                                        <br>
+                                        <label class="control-label col-sm-4">Total</label>
+                                        <div class="col">
+                                            <input type="int" class="form-control"
+                                                placeholder="Otomatis Generate hasil dari penjualan diatas"
+                                                name="id_konsesi" required>
                                         </div>
                                     </div>
                                 </div>
 
-                                <div class="card mb-4 py-3">
-                                    <div class="card-body">
-                                        <label class="control-label col-sm-4">Nama Panel</label>
-                                        <div class="col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Masukkan Nama Panel"
-                                                name="nama_panel" required>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="card mb-4 py-3">
-                                    <div class="card-body">
-                                        <label class="control-label col-sm-4">Unit</label>
-                                        <div class="col-sm-8">
-                                            <input type="int" class="form-control" placeholder="Masukkan Unit"
-                                                name="unit" required>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="card mb-4 py-3">
-                                    <div class="card-body">
-                                        <label class="control-label col-sm-4">Jenis</label>
-                                        <div class="col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Masukkan Jenis"
-                                                name="jenis" required>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="card mb-4 py-3">
-                                    <div class="card-body">
-                                        <label class="control-label col-sm-4">No RPB</label>
-                                        <div class="col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Masukkan No RPB"
-                                                name="no_rpb" required>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="card mb-4 py-3">
-                                    <div class="card-body">
-                                        <label class="control-label col-sm-4">NO PO</label>
-                                        <div class="col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Masukkan No PO"
-                                                name="no_po" required>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-
-
-
-                            <!-- Border Right -->
-                            <div class="col-lg-6">
-
-                                <div class="card mb-4 py-3">
-                                    <div class="card-body">
-                                        <label class="control-label col-sm-4">Kode Material</label>
-                                        <div class="col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Masukkan Kode Material"
-                                                name="kode_material" required>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="card mb-4 py-3">
-                                    <div class="card-body">
-                                        <label class="control-label col-sm-4">Konsesi</label>
-                                        <div class="col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Masukkan Konsesi"
-                                                name="konsesi" required>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="card mb-4 py-3">
-                                    <div class="card-body">
-                                        <label class="control-label col-sm-4">Jml</label>
-                                        <div class="col-sm-8">
-                                            <input type="int" class="form-control" placeholder="Masukkan JML"
-                                                name="jumlah" required>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="card mb-4 py-3">
-                                    <div class="card-body">
-                                        <label class="control-label col-sm-4">NO LKPJ</label>
-                                        <div class="col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Masukkan No LKPJ"
-                                                name="no_lkpj" required>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="card mb-4 py-3">
-                                    <div class="card-body">
-                                        <label class="control-label col-sm-4">Status</label>
-                                        <div class="col-sm-8">
-                                            <select name="status" id="akses_id" class="form-control" required <option
-                                                value="Open">Open</option>
-                                                <option value="Close">Close</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="card mb-4 py-3">
-                                    <div class="card-body">
-                                        <label class="control-label col-sm-4">TGL Matrial Datang</label>
-                                        <div class="col-sm-8">
-                                            <input type="date" class="date-end ml-5 form-control datepicker col-sm-8"
-                                                placeholder="Masukkan Tanggal Datang" name="tgl_matrial_dtg" required>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="card mb-4 py-3">
-                                    <div class="card-body">
-                                        <label class="control-label col-sm-4">TGL Pasang</label>
-                                        <div class="col-sm-8">
-                                            <input type="date" class="date-end ml-5 form-control datepicker col-sm-8"
-                                                placeholder="Masukkan Tanggal Pasang" name="tgl_pasang" required>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="card mb-4 py-3">
-                                    <div class="card-body">
-                                        <label class="control-label col-sm-4">Keterangan</label>
-                                        <div class="col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Masukkan Keterangan"
-                                                name="keterangan" required>
-                                        </div>
-                                    </div>
-                                </div>
 
 
                             </div>
