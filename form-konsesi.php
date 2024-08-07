16abe43928bb6de01c2427ed2d56305cbf355f9f
Mardhyah <mardhyahfathaniaizzati9d@gmail.com>
2024-08-07 09:12:11 +0700

    test

--- form-konsesi.php
+++ form-konsesi.php
@@ -19,9 +19,7 @@ require_once 'crud-konsesi.php';
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
@@ -35,7 +33,7 @@ require_once 'crud-konsesi.php';
     <div id="wrapper">
 
         <!-- Ini Sidebar -->
-        <?php include ("sidebar.php") ?>
+        <?php include("sidebar.php") ?>
         <!-- Batas Akhir Sidebar -->
 
         <!-- Content Wrapper -->
@@ -45,7 +43,7 @@ require_once 'crud-konsesi.php';
             <div id="content">
 
                 <!-- Topbar -->
-                <?php include ("top-bar.php") ?>
+                <?php include("top-bar.php") ?>
 
                 <!-- End of Topbar -->
 
@@ -53,7 +51,7 @@ require_once 'crud-konsesi.php';
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
-                    <h1 class="h3 mb-2 text-gray-800">Form Konsesi</h1>
+                    <h1 class="h3 mb-2 text-gray-800">test Form Konsesi</h1>
                     <p class="mb-4">Mohon Isi Data Konsesi pada Form di Bawah ini.
                     </p>
 
@@ -68,8 +66,7 @@ require_once 'crud-konsesi.php';
                                     <div class="card-body">
                                         <label class="control-label col-sm-4">NO</label>
                                         <div class="col-sm-8">
-                                            <input type="int" class="form-control" placeholder="Masukkan No"
-                                                name="id_konsesi" required>
+                                            <input type="int" class="form-control" placeholder="Masukkan No" name="id_konsesi" required>
                                         </div>
                                     </div>
                                 </div>
@@ -78,8 +75,7 @@ require_once 'crud-konsesi.php';
                                     <div class="card-body">
                                         <label class="control-label col-sm-4">JO</label>
                                         <div class="col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Masukkan JO" name="jo"
-                                                required>
+                                            <input type="text" class="form-control" placeholder="Masukkan JO" name="jo" required>
                                         </div>
                                     </div>
                                 </div>
@@ -88,8 +84,7 @@ require_once 'crud-konsesi.php';
                                     <div class="card-body">
                                         <label class="control-label col-sm-4">WO</label>
                                         <div class="col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Masukkan WO" name="wo"
-                                                required>
+                                            <input type="text" class="form-control" placeholder="Masukkan WO" name="wo" required>
                                         </div>
                                     </div>
                                 </div>
@@ -98,8 +93,7 @@ require_once 'crud-konsesi.php';
                                     <div class="card-body">
                                         <label class="control-label col-sm-4">Nama Project</label>
                                         <div class="col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Masukkan Nama Project"
-                                                name="nama_project" required>
+                                            <input type="text" class="form-control" placeholder="Masukkan Nama Project" name="nama_project" required>
                                         </div>
                                     </div>
                                 </div>
@@ -108,8 +102,7 @@ require_once 'crud-konsesi.php';
                                     <div class="card-body">
                                         <label class="control-label col-sm-4">Nama Panel</label>
                                         <div class="col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Masukkan Nama Panel"
-                                                name="nama_panel" required>
+                                            <input type="text" class="form-control" placeholder="Masukkan Nama Panel" name="nama_panel" required>
                                         </div>
                                     </div>
                                 </div>
@@ -118,8 +111,7 @@ require_once 'crud-konsesi.php';
                                     <div class="card-body">
                                         <label class="control-label col-sm-4">Unit</label>
                                         <div class="col-sm-8">
-                                            <input type="int" class="form-control" placeholder="Masukkan Unit"
-                                                name="unit" required>
+                                            <input type="int" class="form-control" placeholder="Masukkan Unit" name="unit" required>
                                         </div>
                                     </div>
                                 </div>
@@ -128,8 +120,7 @@ require_once 'crud-konsesi.php';
                                     <div class="card-body">
                                         <label class="control-label col-sm-4">Jenis</label>
                                         <div class="col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Masukkan Jenis"
-                                                name="jenis" required>
+                                            <input type="text" class="form-control" placeholder="Masukkan Jenis" name="jenis" required>
                                         </div>
                                     </div>
                                 </div>
@@ -138,8 +129,7 @@ require_once 'crud-konsesi.php';
                                     <div class="card-body">
                                         <label class="control-label col-sm-4">No RPB</label>
                                         <div class="col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Masukkan No RPB"
-                                                name="no_rpb" required>
+                                            <input type="text" class="form-control" placeholder="Masukkan No RPB" name="no_rpb" required>
                                         </div>
                                     </div>
                                 </div>
@@ -148,8 +138,7 @@ require_once 'crud-konsesi.php';
                                     <div class="card-body">
                                         <label class="control-label col-sm-4">NO PO</label>
                                         <div class="col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Masukkan No PO"
-                                                name="no_po" required>
+                                            <input type="text" class="form-control" placeholder="Masukkan No PO" name="no_po" required>
                                         </div>
                                     </div>
                                 </div>
@@ -165,8 +154,7 @@ require_once 'crud-konsesi.php';
                                     <div class="card-body">
                                         <label class="control-label col-sm-4">Kode Material</label>
                                         <div class="col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Masukkan Kode Material"
-                                                name="kode_material" required>
+                                            <input type="text" class="form-control" placeholder="Masukkan Kode Material" name="kode_material" required>
                                         </div>
                                     </div>
                                 </div>
@@ -175,8 +163,7 @@ require_once 'crud-konsesi.php';
                                     <div class="card-body">
                                         <label class="control-label col-sm-4">Konsesi</label>
                                         <div class="col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Masukkan Konsesi"
-                                                name="konsesi" required>
+                                            <input type="text" class="form-control" placeholder="Masukkan Konsesi" name="konsesi" required>
                                         </div>
                                     </div>
                                 </div>
@@ -185,8 +172,7 @@ require_once 'crud-konsesi.php';
                                     <div class="card-body">
                                         <label class="control-label col-sm-4">Jml</label>
                                         <div class="col-sm-8">
-                                            <input type="int" class="form-control" placeholder="Masukkan JML"
-                                                name="jumlah" required>
+                                            <input type="int" class="form-control" placeholder="Masukkan JML" name="jumlah" required>
                                         </div>
                                     </div>
                                 </div>
@@ -195,8 +181,7 @@ require_once 'crud-konsesi.php';
                                     <div class="card-body">
                                         <label class="control-label col-sm-4">NO LKPJ</label>
                                         <div class="col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Masukkan No LKPJ"
-                                                name="no_lkpj" required>
+                                            <input type="text" class="form-control" placeholder="Masukkan No LKPJ" name="no_lkpj" required>
                                         </div>
                                     </div>
                                 </div>
@@ -205,8 +190,7 @@ require_once 'crud-konsesi.php';
                                     <div class="card-body">
                                         <label class="control-label col-sm-4">Status</label>
                                         <div class="col-sm-8">
-                                            <select name="status" id="akses_id" class="form-control" required <option
-                                                value="Open">Open</option>
+                                            <select name="status" id="akses_id" class="form-control" required <option value="Open">Open</option>
                                                 <option value="Close">Close</option>
                                             </select>
                                         </div>
@@ -217,8 +201,7 @@ require_once 'crud-konsesi.php';
                                     <div class="card-body">
                                         <label class="control-label col-sm-4">TGL Matrial Datang</label>
                                         <div class="col-sm-8">
-                                            <input type="date" class="date-end ml-5 form-control datepicker col-sm-8"
-                                                placeholder="Masukkan Tanggal Datang" name="tgl_matrial_dtg" required>
+                                            <input type="date" class="date-end ml-5 form-control datepicker col-sm-8" placeholder="Masukkan Tanggal Datang" name="tgl_matrial_dtg" required>
                                         </div>
                                     </div>
                                 </div>
@@ -227,8 +210,7 @@ require_once 'crud-konsesi.php';
                                     <div class="card-body">
                                         <label class="control-label col-sm-4">TGL Pasang</label>
                                         <div class="col-sm-8">
-                                            <input type="date" class="date-end ml-5 form-control datepicker col-sm-8"
-                                                placeholder="Masukkan Tanggal Pasang" name="tgl_pasang" required>
+                                            <input type="date" class="date-end ml-5 form-control datepicker col-sm-8" placeholder="Masukkan Tanggal Pasang" name="tgl_pasang" required>
                                         </div>
                                     </div>
                                 </div>
@@ -237,8 +219,7 @@ require_once 'crud-konsesi.php';
                                     <div class="card-body">
                                         <label class="control-label col-sm-4">Keterangan</label>
                                         <div class="col-sm-8">
-                                            <input type="text" class="form-control" placeholder="Masukkan Keterangan"
-                                                name="keterangan" required>
+                                            <input type="text" class="form-control" placeholder="Masukkan Keterangan" name="keterangan" required>
                                         </div>
                                     </div>
                                 </div>
@@ -249,8 +230,7 @@ require_once 'crud-konsesi.php';
                         </div>
 
                         <div class="text-center">
-                            <button type="submit" name="submit" class="btn btn-primary waves-effect waves-light"
-                                id="btn-submit">Simpan</button>
+                            <button type="submit" name="submit" class="btn btn-primary waves-effect waves-light" id="btn-submit">Simpan</button>
                         </div>
                         <input type="hidden" name="action" id="action" value="event_dialog_add_newpartnerdata" required>
                     </form>
@@ -262,7 +242,7 @@ require_once 'crud-konsesi.php';
             <!-- End of Main Content -->
 
             <!-- Footer -->
-            <?php include ("footer.php") ?>
+            <?php include("footer.php") ?>
             <!-- End of Footer -->
 
         </div>
